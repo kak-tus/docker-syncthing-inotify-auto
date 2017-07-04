177a452a08e26113b2d337125d3d16aaa339bf9c
Andrey Kuzmin <prepodam@mail.ru>
2017-07-04 14:48:59 +0300

    Run fixes.

--- start.sh
+++ start.sh
@@ -5,8 +5,10 @@ groupdel user 2>/dev/null
 groupadd -g $USER_GID user
 useradd -d /home/user -g user -u $USER_UID user
 
+chown -R user:user /home/user
+
 gosu user consul-template -config /etc/syncthing.hcl &
 child=$!
 
-trap "kill $child" SIGTERM SIGINT
+trap "kill $child" TERM INT
 wait "$child"
